b45ebd061684d454622c07bd7a28d6c7e78e9467
裕斗 近藤 <hilotono@icloud.com>
2023-11-16 10:04:19 +0900

    so

--- SlectScene.cpp
+++ SlectScene.cpp
@@ -1,1 +1,47 @@
 #include "SlectScene.h"
+
+SlectScene* SlectScene::GetInstance()
+{
+	static SlectScene instance;
+	return &instance;
+}
+
+void SlectScene::Initilize()
+{
+	uint32_t tex =
+		TextureManager::LoadTexture("Resources/uvChecker.png");
+	SlectScene::GetInstance()->texHandle= tex;
+
+	for (int i = 0; i < SCENECHANGE_SPRITE_MAX; i++)
+	{
+		SlectScene::GetInstance()->sprite_[i] = make_unique<Sprite>();
+		SlectScene::GetInstance()->sprite_[i]->SetTexHandle(tex);
+		SlectScene::GetInstance()->sprite_[i]->Initialize(new SpriteBoxState, { 0,0 }, { 128,128 });
+	}
+
+	int width = 1;
+	int hegth = 1;
+
+}
+
+void SlectScene::Updata()
+{
+	for (int i = 0; i < SCENECHANGE_SPRITE_MAX; i++)
+	{
+		SlectScene::GetInstance()->DrawFlag = true;
+	}
+
+}
+
+void SlectScene::Draw()
+{
+	if (SlectScene::GetInstance()->DrawFlag)
+	{
+		for (int i = 0; i < SCENECHANGE_SPRITE_MAX; i++)
+		{
+			SlectScene::GetInstance()->worldTransform_[i].UpdateMatrix();
+			SlectScene::GetInstance()->sprite_[i]->Draw(SlectScene::GetInstance()->worldTransform_[i]);
+		}
+	}
+
+}
