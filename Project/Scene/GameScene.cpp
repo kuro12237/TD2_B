2f6d6f34c3728492106b6e05bd8ab7eb3a40ead9
kuro12237 <115063113+kuro12237@users.noreply.github.com>
2023-11-30 04:21:03 +0900

    comp

--- Project/Scene/GameScene.cpp
+++ Project/Scene/GameScene.cpp
@@ -167,12 +167,12 @@ void GameScene::Initialize()
 
 	stageButtonHomeSprite_ = make_unique<Sprite>();
 	stageButtonHomeSprite_->SetTexHandle(UITex);
-	stageButtonHomeSprite_->Initialize(new SpriteBoxState, { 0,0 }, { 593.0f,152.0f });
+	stageButtonHomeSprite_->Initialize(new SpriteBoxState, { 0,0 }, { 648.0f,152.0f });
 			   
 	stageButtonHomeWorldTransform_.Initialize();
-	stageButtonHomeWorldTransform_.scale = { 0.35f,0.35f,0.35f };
+	stageButtonHomeWorldTransform_.scale = { 0.3f,0.3f,0.3f };
 	stageButtonHomeWorldTransform_.translate.y = 636.8f + Height;
-	stageButtonHomeWorldTransform_.translate.x = 92.0f + width + 10.0f;
+	stageButtonHomeWorldTransform_.translate.x = 97.0f + width + 4.0f;
 	stageButtonHomeWorldTransform_.UpdateMatrix();
 
 #pragma endregion
